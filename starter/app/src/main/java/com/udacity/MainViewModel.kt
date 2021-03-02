dc1e21dffc1f44d08690979dc59ada29de85773d
Adam Giergun <adam.giergun@gmail.com>
2021-03-02 18:28:51 +0100

    Implement ViewModel for MainActivity

---
+++ starter/app/src/main/java/com/udacity/MainViewModel.kt
@@ -0,0 +1,42 @@
+package com.udacity
+
+import android.app.Application
+import android.app.DownloadManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.AndroidViewModel
+
+
+class MainViewModel(application: Application): AndroidViewModel(application) {
+
+    val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+        }
+    }
+
+    private val app = getApplication<Application>()
+    private var downloadID: Long = 0
+
+    fun download() {
+        val request =
+            DownloadManager.Request(Uri.parse(URL))
+                .setTitle(app.getString(R.string.app_name))
+                .setDescription(app.getString(R.string.app_description))
+                .setRequiresCharging(false)
+                .setAllowedOverMetered(true)
+                .setAllowedOverRoaming(true)
+
+        val downloadManager = app.getSystemService(AppCompatActivity.DOWNLOAD_SERVICE) as DownloadManager
+        downloadID =
+            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+    }
+
+    companion object {
+        private const val URL =
+            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+    }
+}
