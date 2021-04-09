2c9887d9ce1f803df4694c5ce8a5ae209ffd2f52
Adam Giergun <adam.giergun@gmail.com>
2021-04-09 22:54:12 +0200

    Add data to content_detail.xml

--- starter/app/src/main/java/com/udacity/BindingAdapters.kt
+++ starter/app/src/main/java/com/udacity/BindingAdapters.kt
@@ -1,8 +1,24 @@
 package com.udacity
 
+import android.app.DownloadManager.*
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("buttonState")
 fun LoadingButton.setButtonState(buttonState: ButtonState) {
     setState(buttonState)
+}
+
+@BindingAdapter("downloadStatus")
+fun TextView.setDownloadStateText(downloadStatus: Int) {
+    setText(
+        when (downloadStatus) {
+            STATUS_FAILED -> R.string.failed
+            STATUS_PAUSED -> R.string.paused
+            STATUS_PENDING -> R.string.pending
+            STATUS_RUNNING -> R.string.running
+            STATUS_SUCCESSFUL -> R.string.successful
+            else -> R.string.unknown
+        }
+    )
 }
