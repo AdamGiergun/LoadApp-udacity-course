b8e99951f075fe05f9669d4ed275b33f32c0a2a4
Adam Giergun <adam.giergun@gmail.com>
2021-04-13 19:04:22 +0200

    Add getting notification id for cancelling notification after opening DetailActivity

--- starter/app/src/main/java/com/udacity/DetailActivity.kt
+++ starter/app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,7 +1,9 @@
 package com.udacity
 
+import android.app.NotificationManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.udacity.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
@@ -14,5 +16,12 @@ class DetailActivity : AppCompatActivity() {
             val download: Download? = intent.getParcelableExtra("download")
             download?.let { contentDetail.download = it }
         }
+
+        val notificationId = intent.getIntExtra("notification_id", 0)
+        val notificationManager = ContextCompat.getSystemService(
+                this,
+                NotificationManager::class.java
+            ) as NotificationManager
+        if (notificationId > 0) notificationManager.cancel(notificationId)
     }
 }
