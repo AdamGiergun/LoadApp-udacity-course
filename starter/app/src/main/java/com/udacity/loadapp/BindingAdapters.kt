610231cffdac30a91ed23cdec59aeba0d5c9b7cd
Adam Giergun <adasss@poczta.onet.pl>
2022-06-30 15:53:09 +0200

    Change info shown in notification

--- starter/app/src/main/java/com/udacity/loadapp/BindingAdapters.kt
+++ starter/app/src/main/java/com/udacity/loadapp/BindingAdapters.kt
@@ -15,16 +15,16 @@ fun LoadingButton.setButtonState(buttonState: ButtonState) {
 
 @BindingAdapter("downloadStatus")
 fun TextView.setDownloadStateText(downloadStatus: Int) {
-    setText(
-        when (downloadStatus) {
-            STATUS_FAILED -> R.string.failed
-            STATUS_PAUSED -> R.string.paused
-            STATUS_PENDING -> R.string.pending
-            STATUS_RUNNING -> R.string.running
-            STATUS_SUCCESSFUL -> R.string.successful
-            else -> R.string.unknown
-        }
-    )
+    setText(getDownloadStatusStringId(downloadStatus))
+}
+
+fun getDownloadStatusStringId(downloadStatus: Int) = when (downloadStatus) {
+    STATUS_FAILED -> R.string.failed
+    STATUS_PAUSED -> R.string.paused
+    STATUS_PENDING -> R.string.pending
+    STATUS_RUNNING -> R.string.running
+    STATUS_SUCCESSFUL -> R.string.successful
+    else -> R.string.unknown
 }
 
 @BindingAdapter("isCustomUrlSelected")
--- starter/app/src/main/java/com/udacity/loadapp/notification/LoadAppNotification.kt
+++ starter/app/src/main/java/com/udacity/loadapp/notification/LoadAppNotification.kt
@@ -5,7 +5,9 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.udacity.loadapp.Download
 import com.udacity.loadapp.R
+import com.udacity.loadapp.getDownloadStatusStringId
 import kotlin.math.abs
 import kotlin.random.Random
 
@@ -20,12 +22,28 @@ object LoadAppNotification {
 
     fun notify(context: Context, intent: Intent) {
         intent.putExtra(EXTRA_ID, notificationId)
-        val pendingIntent = PendingIntent.getActivity(context, notificationId, intent, PendingIntent.FLAG_IMMUTABLE)
+        val pendingIntent =
+            PendingIntent.getActivity(context, notificationId, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val download: Download? = intent.getParcelableExtra(Download.EXTRA_NAME)
 
         NotificationCompat.Builder(context, LoadAppNotificationChannel.CHANNEL_ID).apply {
             setSmallIcon(R.drawable.ic_assistant_black_24dp)
             setContentTitle(context.getString(R.string.notification_title))
-            setContentText(context.getString(R.string.notification_description))
+            setStyle(
+                NotificationCompat.BigTextStyle().bigText(
+                    context.getString(
+                        R.string.notification_description,
+                        context.getString(download?.details ?: R.string.unknown),
+                        context.getString(
+                            if (download == null)
+                                R.string.unknown
+                            else
+                                getDownloadStatusStringId(download.status)
+                        )
+                    )
+                )
+            )
             priority = NotificationCompat.PRIORITY_DEFAULT
             setAutoCancel(true)
             addAction(
